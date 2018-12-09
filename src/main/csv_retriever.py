44da81fe4ee86b04c4ebb4bc75e21989a44ad0c9
trug <tehseen.rug@gmail.com>
2018-12-09 20:49:33 +0100

    refactor retriever

--- src/main/csv_retriever.py
+++ src/main/csv_retriever.py
@@ -7,17 +7,19 @@ class CSVRetriever:
 
     def retrieve(self):
         data_frame = pd.read_csv('../resources/books.csv')
-        print(data_frame)
         count_row = data_frame.shape[0]
-
         data_of_given_row = []
-        for j in range(0, count_row - 1):
-            for i in range(0, len(self.bookAttributes) - 1):
-                print(i)
-                print(self.bookAttributes[i])
-                data_of_given_row.append(data_frame.at[j, self.bookAttributes[i]])
-            self.books_from_csv.append(data_of_given_row)
-            data_of_given_row = []
-        print(self.books_from_csv)
+
+        self.handle_all_rows(count_row, data_frame, data_of_given_row)
         return self.books_from_csv
+
+    def handle_all_rows(self, count_row, data_frame, data_of_given_row):
+        for current_row in range(0, count_row):
+            self.retrieve_book_in_row(current_row, data_frame, data_of_given_row)
+            data_of_given_row = []
+
+    def retrieve_book_in_row(self, current_row, data_frame, data_of_given_row):
+        for current_attribute in range(0, len(self.bookAttributes) - 1):
+            data_of_given_row.append(data_frame.at[current_row, self.bookAttributes[current_attribute]])
+        self.books_from_csv.append(data_of_given_row)
 
