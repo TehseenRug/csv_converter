7863a388dfc6ea3b3691ad665c49ccfb7b7ed602
trug <tehseen.rug@gmail.com>
2018-12-09 21:15:12 +0100

    refactor service

--- src/main/csv_service.py
+++ src/main/csv_service.py
@@ -18,6 +18,7 @@ class CSVService:
 
     all_books = []
     all_books_as_dtos = []
+
     def __init__(self):
         self.csv_retriever = CSVRetriever()
 
@@ -25,20 +26,33 @@ class CSVService:
 
         books_from_csv = self.csv_retriever.retrieve()
         for current_book in range (0, len(books_from_csv)):
-            book = Book()
-            title = Title()
-            author = Author()
-            genre = Genre()
-            title.mainTitle = books_from_csv[current_book][INDEX_OF_MAIN_TITLE]
-            title.subTitle = books_from_csv[current_book][INDEX_OF_SUB_TITLE]
-            author.firstName = books_from_csv[current_book][INDEX_OF_FIRST_NAME]
-            author.lastName = books_from_csv[current_book][INDEX_OF_LAST_NAME]
-            genre.mainGenre = books_from_csv[current_book][INDEX_OF_MAIN_GENRE]
-            genre.subGenre = books_from_csv[current_book][INDEX_OF_SUB_GENRE]
-            book.author = author
-            book.title = title
-            book.genre = genre
-            book.appearanceYear = books_from_csv[current_book][INDEX_OF_APPEARANCE_YEAR]
+            book = self.setup_book(books_from_csv, current_book)
             print(book)
             self.all_books_as_dtos.append(book)
         return self.all_books_as_dtos
+
+    def setup_book(self, books_from_csv, current_book):
+        book = Book()
+        book.author = self.setup_author(books_from_csv, current_book)
+        book.title = self.setup_title(books_from_csv, current_book)
+        book.genre = self.setup_genre(books_from_csv, current_book)
+        book.appearanceYear = books_from_csv[current_book][INDEX_OF_APPEARANCE_YEAR]
+        return book
+
+    def setup_genre(self, books_from_csv, current_book):
+        genre = Genre()
+        genre.mainGenre = books_from_csv[current_book][INDEX_OF_MAIN_GENRE]
+        genre.subGenre = books_from_csv[current_book][INDEX_OF_SUB_GENRE]
+        return genre
+
+    def setup_author(self, books_from_csv, current_book):
+        author = Author()
+        author.firstName = books_from_csv[current_book][INDEX_OF_FIRST_NAME]
+        author.lastName = books_from_csv[current_book][INDEX_OF_LAST_NAME]
+        return author
+
+    def setup_title(self, books_from_csv, current_book):
+        title = Title()
+        title.mainTitle = books_from_csv[current_book][INDEX_OF_MAIN_TITLE]
+        title.subTitle = books_from_csv[current_book][INDEX_OF_SUB_TITLE]
+        return title
