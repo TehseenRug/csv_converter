cdec411413a300494c718a1a33deff5bf8d9a9ab
trug <tehseen.rug@gmail.com>
2018-12-29 10:06:30 +0100

    do not accumulate data on multiple requests

--- src/main/controller.py
+++ src/main/controller.py
@@ -2,13 +2,13 @@ from flask import Flask, json
 
 from src.main.csv_service import CSVService
 
+csv_service = CSVService()
 app = Flask(__name__)
+books = csv_service.get_books_from_csv()
 
 
 @app.route('/')
 def get_books_from_csv():
-    csv_service = CSVService()
-    books = csv_service.get_books_from_csv()
     return json.dumps(books, default=lambda x: x.__dict__)
 
 
